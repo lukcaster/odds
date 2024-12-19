7b89a6b8e3da2651707226a5ef60e9494da4d7e5
lukgru <lukasz.gruchala@fitech.pl>
2024-12-19 13:51:49 +0100

    modified api a bit

---
+++ src/app/get-odds/get-us-nfl-odds.ts
@@ -0,0 +1,8 @@
+import {APIResponse, Page} from "playwright";
+import {Sport} from "../utils/enums/sport";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export async function getOdds(page: Page, sport: Sport): Promise<APIResponse> {
+    return await page.request.get(`https://api.the-odds-api.com/v4/sports/${sport.valueOf()}/odds/?apiKey=${process.env.apiKey}&regions=us&markets=h2h,spreads&oddsFormat=decimal`)
+}
