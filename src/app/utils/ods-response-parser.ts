24537576821e41130fdd98700391399cbb4070d6
lukgru <lukasz.gruchala@fitech.pl>
2024-12-19 13:51:41 +0100

    added parser

---
+++ src/app/utils/ods-response-parser.ts
@@ -0,0 +1,31 @@
+import {z} from "zod";
+
+export const odsResponseParser = z.object({
+    id: z.string(),
+    sport_key: z.string(),
+    sport_title: z.string(),
+    commence_time: z.string(),
+    home_team: z.string(),
+    away_team: z.string(),
+    bookmakers: z.array(
+        z.object({
+            key: z.string(),
+            title: z.string(),
+            last_update: z.string(),
+            markets: z.array(
+                z.object({
+                    key:z.string(),
+                    last_update: z.string(),
+                    outcomes: z.array(
+                        z.object({
+                            name: z.string(),
+                            price: z.number()
+                        })
+                    )
+                })
+            )
+        })
+    )
+})
+
+export type OdsResponse = z.infer<typeof odsResponseParser>
