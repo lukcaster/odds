13686985a5b2137f152d55208c05221b2c3a289d
lukgru <lukasz.gruchala@fitech.pl>
2024-12-20 07:45:03 +0100

    key from .env does not work

--- src/app/get-odds/get-us-nfl-odds.ts
+++ src/app/get-odds/get-us-nfl-odds.ts
@@ -1,8 +1,9 @@
 import {APIResponse, Page} from "playwright";
 import {Sport} from "../utils/enums/sport";
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 export async function getOdds(page: Page, sport = Sport.NFL): Promise<APIResponse> {
-    return await page.request.get(`https://api.the-odds-api.com/v4/sports/${sport.valueOf()}/odds/?apiKey=${process.env.apiKey}&regions=us&markets=h2h,spreads&oddsFormat=decimal`)
+    return await page.request.get(`https://api.the-odds-api.com/v4/sports/${sport.valueOf()}/odds/?apiKey=4e202ad5763c7d6fee3e2368dae804ba&regions=us&markets=spreads&oddsFormat=decimal`)
 }
