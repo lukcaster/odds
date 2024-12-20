d30613419c5f1f7be64e3f0c3dcbf0b46c229d2b
lukgru <lukasz.gruchala@fitech.pl>
2024-12-20 07:45:14 +0100

    modified parser

--- src/app/utils/ods-response-parser.ts
+++ src/app/utils/ods-response-parser.ts
@@ -1,31 +1,23 @@
 import {z} from "zod";
 
-export const odsResponseParser = z.object({
+export const odsResponseParser = z.array(
+    z.object({
     id: z.string(),
     sport_key: z.string(),
     sport_title: z.string(),
     commence_time: z.string(),
     home_team: z.string(),
     away_team: z.string(),
-    bookmakers: z.array(
-        z.object({
-            key: z.string(),
-            title: z.string(),
-            last_update: z.string(),
-            markets: z.array(
-                z.object({
-                    key:z.string(),
-                    last_update: z.string(),
-                    outcomes: z.array(
-                        z.object({
-                            name: z.string(),
-                            price: z.number()
-                        })
-                    )
-                })
-            )
-        })
-    )
-})
+    bookmakers: z.array(z.object({
+        key: z.string(),
+        title: z.string(),
+        last_update: z.string(),
+        markets: z.array(z.object({
+            key: z.string(), last_update: z.string(), outcomes: z.array(z.object({
+                name: z.string(), price: z.number(), point: z.number()
+            }))
+        }))
+    }))
+}))
 
 export type OdsResponse = z.infer<typeof odsResponseParser>
