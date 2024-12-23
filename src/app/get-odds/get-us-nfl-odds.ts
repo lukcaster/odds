c877e870fb6e5070e41d5a1fe1c00b8604b821b8
lukgru <lukasz.gruchala@fitech.pl>
2024-12-23 07:11:34 +0100

    changed to .env variable

--- src/app/get-odds/get-us-nfl-odds.ts
+++ src/app/get-odds/get-us-nfl-odds.ts
@@ -5,5 +5,5 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export async function getOdds(page: Page, sport = Sport.NFL): Promise<APIResponse> {
-    return await page.request.get(`https://api.the-odds-api.com/v4/sports/${sport.valueOf()}/odds/?apiKey=4e202ad5763c7d6fee3e2368dae804ba&regions=us&markets=spreads&oddsFormat=decimal`)
+    return await page.request.get(`https://api.the-odds-api.com/v4/sports/${sport.valueOf()}/odds/?apiKey=${process.env.apiKey}&regions=us&markets=spreads&oddsFormat=decimal`)
 }
